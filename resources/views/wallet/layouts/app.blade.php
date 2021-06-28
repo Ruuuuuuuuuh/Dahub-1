374cc7bb54d3370eae96d9c56526900c83cc5de0
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2021-06-28 11:18:13 +0500

    add mix to css version

--- resources/views/wallet/layouts/app.blade.php
+++ resources/views/wallet/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="/fonts/Gotham/style.css" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
