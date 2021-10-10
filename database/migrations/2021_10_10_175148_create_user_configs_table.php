425f1327dac081f99af079fae604ce8cadc2ae31
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2021-10-10 20:54:34 +0300

    Add user configs model and migration

---
+++ database/migrations/2021_10_10_175148_create_user_configs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_configs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_uid');
+            $table->string('meta');
+            $table->string('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_configs');
+    }
+}
