39b7c3afd8f833239177d1d42aaae96581fe8c88
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-01-13 14:58:27 +0300

    Currency fix bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -121,7 +121,7 @@ class User extends Authenticatable implements Wallet, Confirmable, WalletFloat
                 [
                     'name' => $currency,
                     'slug' => $currency,
-                    'decimal_places' => Currency::where('title', $currency)->findOrfFail()->decimal_places
+                    'decimal_places' => Currency::where('title', $currency)->findOrFail()->decimal_places
                 ]
             );
         }
