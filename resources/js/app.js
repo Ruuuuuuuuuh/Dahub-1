579ec65f3c74f5d6d8758c8114b6f7451c39a3df
alshchetinin <alexsander.qw@gmail.com>
2022-03-30 20:29:19 +0500

    подключил тестовый vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,22 @@
 require('./bootstrap');
 import Vue from 'vue';
+import VueRouter from 'vue-router'
+import routes from './routes';
+import appVue from './components/App'
+
 window.Vue = require('vue');
 
 Vue.component('payments-list', require('./components/PaymentsList.vue').default);
 Vue.component('createorderwithdraw', require('./components/CreateOrderWithdraw.vue').default);
 Vue.component('timeleft', require('./components/timeleft.vue').default);
 
+Vue.use(VueRouter)
+
 const app = new Vue({
     el: '#app-vue'
 });
+
+const newApp = new Vue({
+    render: h => h(appVue),
+    router: routes
+}).$mount("#new-vue-app")
---
+++ resources/views/dashboard/test.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" id="viewport" content="user-scalable=no, initial-scale=1, width=device-width">
+    <title>Da HUB - первая децентрализованная платформа</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/dashboard.css') }}">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#9f00a7">
+    <meta name="theme-color" content="#ffffff">
+
+    <script>
+        window.user = {!! Auth::User()->toJson(JSON_PRETTY_PRINT) !!}
+        window.onload = function () {
+            if (screen.width < 450) {
+                let mvp = document.getElementById('viewport');
+                mvp.setAttribute('content','user-scalable=no,width=450');
+            }
+        }
+    </script>
+</head>
+
+<body>
+<div  class="app" id="new-vue-app" data-mode="{{$mode}}" data-page="{{Route::current()->getName()}}">
+
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(86626209, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/86626209" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</body>
+</html>
