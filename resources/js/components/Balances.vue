39afef048831d1edf3ce3bd70dd00a536f69e0ce
alshchetinin <alexsander.qw@gmail.com>
2022-03-30 21:51:54 +0500

    Добавил компонент балансов

---
+++ resources/js/components/Balances.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="balance">
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="total-amount">
+                        <p class="total-amount-title">0<span>ETH</span></p>
+                        <p class="total-currency">≈ $ <span>0</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="balance-items">
+            <div class="balance-item d-flex justify-content-between w-100">
+                <div class="balance-item-currency d-flex align-items-center">
+                    <div class="balance-item-currency-icon">
+                        <div class="icon-wrapper">
+                            icon
+                        </div>
+                    </div>
+                    <div class="balance-item-currency-text">
+                        <div class="balance-item-currency-title">
+                            titleWallet(ETH)
+                        </div>
+                        <div class="balance-item-currency-description">
+                            Ethereum
+                        </div>
+                    </div>
+                </div>
+                <div class="balance-item-amount">
+                    <div class="balance-item-amount-title">
+                        balance
+                    </div>
+                    <div class="balance-item-amount-description">
+                        $ 00
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
