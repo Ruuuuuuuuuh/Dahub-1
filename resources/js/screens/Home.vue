1cde26a01a4b75e4c73a23142030ced71bce32ef
alshchetinin <alexsander.qw@gmail.com>
2022-03-30 21:53:29 +0500

    подключил бургер меню

--- resources/js/screens/Home.vue
+++ resources/js/screens/Home.vue
@@ -1,19 +1,31 @@
 <template>
-    <home-layout>
-
-        1./
-
-    </home-layout>
+    <div>
+        <home-layout> </home-layout>
+        <transition appear name="slideLeft">
+            <BurgerNavigation
+                v-if="this.$store.state.showBurgerMenu"
+            ></BurgerNavigation>
+        </transition>
+    </div>
 </template>
 
 <script>
-import HomeLayout from '../layout/HomeLayout.vue'
+import HomeLayout from "../layout/HomeLayout";
+import BurgerNavigation from "../components/BurgerNavigation";
 export default {
-    components: { HomeLayout },
-
-}
+    components: { HomeLayout, BurgerNavigation },
+    data() {
+        return {};
+    }
+};
 </script>
 
-<style>
-
+<style scoped>
+.slideLeft-enter-active,
+.slideLeft-leave-active {
+    transition: transform 0.5s cubic-bezier(0.76, 0, 0.24, 1);
+}
+.slideLeft-enter, .slideLeft-leave-to {
+    transform: translateX(-100%);
+}
 </style>
