649fe5ddb32d732bbc298af4a27fe4b91233dfd4
alshchetinin <alexsander.qw@gmail.com>
2022-03-31 17:40:45 +0500

    Обновил роуты

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,21 +1,31 @@
 import VueRouter from "vue-router";
 import Home from "./screens/Home"
 import CreateOrder from "./screens/CreateOrder"
+import CreateOrderDeposit from "./screens/CreateOrderDeposit"
 import Settings from "./screens/Settings"
 
 export default new VueRouter({
     routes: [
         {
             path: '',
-            component: Home
+            component: Home,
+            name: 'home'
         },
         {
             path: '/create-order',
-            component: CreateOrder
+            component: CreateOrder,
+            name: 'create-order',
+            children: [
+                {
+                    path: 'deposit',
+                    component: CreateOrderDeposit,
+                }
+            ]
         },
         {
             path: '/settings',
-            component: Settings
+            component: Settings,
+            name: 'settings'
         }
     ],
     // mode: 'history'
