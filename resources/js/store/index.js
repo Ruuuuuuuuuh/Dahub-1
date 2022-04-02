16587633bb77290746fd4e5bcd0670fd995ac8bc
alshchetinin <alexsander.qw@gmail.com>
2022-04-02 12:29:01 +0500

    Добавил данные для юзера

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import naviagation from './modules/naviagation'
+import user from './modules/user'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        naviagation
+        naviagation,
+        user
     }
 });
