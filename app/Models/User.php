ed66dffe2715dbe6c55210ceb8eccb2dbe919ed7
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-04-11 01:06:14 +0300

    Пофиксил баг, когда в токенсейле отображались заявки на ввод и вывод из валлета

--- app/Models/User.php
+++ app/Models/User.php
@@ -122,7 +122,7 @@ class User extends Authenticatable implements Wallet, Confirmable, WalletFloat
      */
     public function hasActiveOrder(): bool
     {
-        return $this->orders()->where('status', '!=', 'completed')->exists();
+        return $this->orders()->where('status', '!=', 'completed')->where('destination', 'TokenSale')->exists();
     }
 
 
