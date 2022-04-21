267a2f6d9107445d9775d824dd034a4e2f3a07af
alshchetinin <alexsander.qw@gmail.com>
2022-04-21 20:46:12 +0500

    убрал тестовый vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,6 @@
 require('./bootstrap');
 import Vue from 'vue';
-import VueRouter from 'vue-router'
-import routes from './routes';
 import store from './store/index'
-import appVue from './components/App'
 import Vue2TouchEvents from 'vue2-touch-events'
 import VueDraggableResizable from 'vue-draggable-resizable'
 
@@ -14,7 +11,6 @@ Vue.component('createorderwithdraw', require('./components/CreateOrderWithdraw.v
 Vue.component('timeleft', require('./components/timeleft.vue').default);
 Vue.component('app', require('./components/App').default);
 
-Vue.use(VueRouter)
 Vue.use(Vue2TouchEvents)
 
 window.Vue = require('vue');
@@ -22,16 +18,8 @@ window.Vue = require('vue');
 
 if(document.querySelector('#app-vue')) {
 const app = new Vue({
+    store,
     el: '#app-vue'
 });
 }
 
-
-
-if(document.querySelector('#new-vue-app')) {
-const newApp = new Vue({
-    router: routes,
-    store,
-    el: "#new-vue-app"
-})
-}
