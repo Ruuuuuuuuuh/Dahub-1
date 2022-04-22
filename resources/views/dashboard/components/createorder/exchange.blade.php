b5fd3516ec4fee2aeeea690828f84c241c75b483
alshchetinin <alexsander.qw@gmail.com>
2022-04-22 13:46:52 +0500

    убрал на время обмен

--- resources/views/dashboard/components/createorder/exchange.blade.php
+++ resources/views/dashboard/components/createorder/exchange.blade.php
@@ -1,4 +1,4 @@
-<createOrderExchange
+{{-- <createOrderExchange
 :currencies="{{ \App\Models\Currency::with('payments')->get()}}"
 _token="{{ csrf_token() }}"
-></createOrderExchange>
+></createOrderExchange> --}}
