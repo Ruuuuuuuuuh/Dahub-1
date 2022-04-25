4950898fbb05ce579c22e898a26b810fe82a5358
alshchetinin <alexsander.qw@gmail.com>
2022-04-25 21:08:59 +0500

    Исправил баг с увеличением кнопки указания кошелька

--- resources/js/components/CreateOrderWithdraw.vue
+++ resources/js/components/CreateOrderWithdraw.vue
@@ -43,9 +43,27 @@
         </div>
     </div>
     <div class="form-control address">
-        <label for="address">{{ this.crypto ? 'Выберите кошелек' : 'Выберите карту'}}</label>
+        <label for="address">{{ this.crypto ? 'Номер кошелька' : 'Номер карты'}}</label>
         <div class="select-wrapper" @click="paymentsDeatails()">
-            <input type="button" name="address" class="input-address" value="" v-model="address" @change="checkValidate()"/>
+            <a class="addres-btn" href="#">
+                <span v-if="!address"> {{ crypto ? 'Выбрать кошелек' : 'Выбрать карту'}} </span>
+                    <div v-if="address" class="address-text">
+                        <div>
+                            {{address}}
+                        </div>
+
+                        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M2.50871 15.0002H5.85407C6.10498 15.0002 6.27224 14.9168 6.43951 14.7502L15.6393 5.5835C15.9738 5.25016 15.9738 4.75016 15.6393 4.41683L12.2939 1.0835C11.9594 0.750163 11.4576 0.750163 11.123 1.0835L1.92327 10.2502C1.756 10.4168 1.67236 10.5835 1.67236 10.8335V14.1668C1.67236 14.6668 2.0069 15.0002 2.50871 15.0002ZM3.34488 11.1668L11.7083 2.8335L13.8828 5.00016L5.51937 13.3335H3.34488V11.1668ZM17.5629 19.1667C18.0647 19.1667 18.3992 18.8333 18.3992 18.3333C18.3992 17.8333 18.0647 17.5 17.5629 17.5H2.50871C2.0069 17.5 1.67236 17.8333 1.67236 18.3333C1.67236 18.8333 2.0069 19.1667 2.50871 19.1667H17.5629Z" fill="black"/>
+                            <mask id="mask0_1505_9645" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="1" y="0" width="18" height="20">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M2.50871 15.0002H5.85407C6.10498 15.0002 6.27224 14.9168 6.43951 14.7502L15.6393 5.5835C15.9738 5.25016 15.9738 4.75016 15.6393 4.41683L12.2939 1.0835C11.9594 0.750163 11.4576 0.750163 11.123 1.0835L1.92327 10.2502C1.756 10.4168 1.67236 10.5835 1.67236 10.8335V14.1668C1.67236 14.6668 2.0069 15.0002 2.50871 15.0002ZM3.34488 11.1668L11.7083 2.8335L13.8828 5.00016L5.51937 13.3335H3.34488V11.1668ZM17.5629 19.1667C18.0647 19.1667 18.3992 18.8333 18.3992 18.3333C18.3992 17.8333 18.0647 17.5 17.5629 17.5H2.50871C2.0069 17.5 1.67236 17.8333 1.67236 18.3333C1.67236 18.8333 2.0069 19.1667 2.50871 19.1667H17.5629Z" fill="white"/>
+                            </mask>
+                            <g mask="url(#mask0_1505_9645)">
+                            <rect width="20.0722" height="20" fill="#0D1F3C"/>
+                            </g>
+                        </svg>
+                    </div>
+                </a>
+            <input type="button" name="address" hidden class="input-address"  v-model="address" @change="checkValidate()"/>
         </div>
     </div>
     <div v-if="showPayments" >
@@ -185,8 +203,8 @@ export default {
 }
 
 .button {
-    background: #347AF0;
-    border-radius: 23px;
+    background: linear-gradient(87.76deg, #85F362 -53.4%, #02AAFF 67.87%);
+    border-radius: 15px;
     width: 100%;
     display: block;
     text-align: center;
@@ -210,5 +228,54 @@ export default {
     color: #E15063;
     margin-top: 12px;
     font-weight: 600;
+}
+
+.address {
+    margin-bottom: 0;
+}
+
+.addres-btn {
+    border-radius: 15px;
+    background: #fff;
+    height: 46px;
+    line-height: 1;
+    border: 2px solid #00aaff;
+    display: flex;
+    color:#0D1F3C;
+    font-weight: 600;
+    justify-content: center;
+    align-items: center;
+    padding: 0px 13px;
+}
+
+.address-text {
+    font-size: 12px;
+    display: grid;
+    grid-template-columns: 1fr 20px;
+    column-gap: 10px;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+    div {
+        display: flex;
+        overflow-x: scroll;
+        padding-right: 20px;
+        }
+    &::after {
+        content: "";
+        position: absolute;
+        right: 30px;
+        bottom: 0;
+        width: 40px;
+        height: 18px;
+        box-sizing: border-box;
+        flex-shrink: 0;
+        background: linear-gradient(270deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%);
+    }
+    @media screen and (max-width: 375px) {
+        font-size: 10px;
+    }
 }
 </style>
